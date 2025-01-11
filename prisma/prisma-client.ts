036c533ae6bda6d042b9eda5ab2261550abde086
Nikita Galkin <nikiprostoff@gmail.com>
2025-01-11 20:26:09 +0500

    feat(prisma-client): Add prisma-client, schema.prisma, change scripts in package.json for prisma

---
+++ prisma/prisma-client.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from '@prisma/client'
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+
+export const prisma = globalForPrisma.prisma || new PrismaClient()
+
+// eslint-disable-next-line node/prefer-global/process
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
